7a7019c2cc6ca108cf2af9e1905fbdb6086747ff
imraulwrites <imraul.dev@gmail.com>
2026-03-30 01:28:44 +0600

    creating the repo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+
+  return (
+    <section>
+      <h1 className="text-6xl font-bold text-center">hello world</h1>
+      <div className="card">
+        <p>hi there</p>
+      </div>
+    </section>
+  );
+}
+
+export default App;
