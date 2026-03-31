9e0c73d89c86d4fc43da7750f8973d8043095fcd
imraulwrites <imraul.dev@gmail.com>
2026-04-01 02:18:13 +0600

    Stats section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Hero from './components/sections/Hero';
+import Stats from './components/sections/Stats';
 
 function App() {
   return (
     <section className="">
       <Navbar />
       <Hero />
+      <Stats />
 
       <Footer />
     </section>
---
+++ src/components/sections/Stats.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Stats() {
+  return (
+    <section className="bg-gradient">
+      <div className="wrapper grid grid-cols-3 gap-10 py-16 text-white text-center">
+        <div className="border-r-2 border-white/30">
+          <h2 className="text-6xl font-extrabold mb-3">50K+</h2>
+          <p className="text-2xl font-medium text-white/80">Active Users</p>
+        </div>
+        <div className="border-r-2 border-white/30">
+          <h2 className="text-6xl font-extrabold mb-3">200K+</h2>
+          <p className="text-2xl font-medium text-white/80">Premium Tools</p>
+        </div>{' '}
+        <div>
+          <h2 className="text-6xl font-extrabold mb-3">4.9</h2>
+          <p className="text-2xl font-medium text-white/80">Rating</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Stats;
