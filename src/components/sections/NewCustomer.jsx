b5f528d6d18da20bce176c869347b7882b8601b6
imraulwrites <imraul.dev@gmail.com>
2026-04-14 03:35:39 +0600

    fixing typo

--- src/components/sections/NewCustomer.jsx
+++ src/components/sections/NewCustomer.jsx
@@ -1,13 +1,15 @@
 import User from '../../assets/user.png';
 import Rocket from '../../assets/rocket.png';
 import Package from '../../assets/package.png';
-import NewCustomerCard from './newCustomerCard';
+import NewCustomerCard from './NewCustomerCard';
 
 function NewCustomer() {
   return (
     <section className="wrapper py-30 text-center flex flex-col items-center justify-center">
-      <h1 className='text-5xl font-extrabold text-heading-dark mb-4'>Get Started in 3 Steps</h1>
-      <p className='text-base font-normal text-muted mb-10'>Start using premium digital tools in minutes, not hours.</p>
+      <h1 className="text-5xl font-extrabold text-heading-dark mb-4">Get Started in 3 Steps</h1>
+      <p className="text-base font-normal text-muted mb-10">
+        Start using premium digital tools in minutes, not hours.
+      </p>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         <div>
           <NewCustomerCard
